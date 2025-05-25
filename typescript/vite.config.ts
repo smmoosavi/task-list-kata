af8b5a0ed992670566d0091088895151f75cb1d0
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2025-05-25 20:39:48 +0330

    add coverage

--- typescript/vite.config.ts
+++ typescript/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
@@ -5,6 +6,18 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    test: {
+        reporters: ['default', 'junit'],
+        outputFile: {
+            junit: 'coverage/report.xml',
+        },
+        coverage: {
+            all: true,
+            provider: 'istanbul',
+            include: ['src/**'],
+            reporter: ['clover', 'json', 'lcov', 'text', 'cobertura'],
+        },
+    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/main.ts'),
